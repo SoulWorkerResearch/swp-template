784dbfe78c68565b37b3d8839aa1b61142f13e32
sawich <sawich@angeldev.org>
2022-01-06 12:05:10 +0200

    Initial D

---
+++ swp-template/headers/swp_template.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <sdk/plugin/plugin_base.hpp>
+
+class swp_template : public sdk::plugin::base<swp_template>
+{
+public:
+  virtual auto main(void) const -> void 
+  {
+    spdlog::info("hello, world!");
+  }
+};
---
+++ swp-template/sources/emplace.cpp
@@ -0,0 +1,16 @@
+// local
+#include "../headers/swp_template.hpp"
+
+// deps
+#include <sdk/plugin/plugin_emplace.hpp>
+
+namespace sdk::plugin
+{
+  extern "C" __declspec(dllexport) auto emplace(void)->info* {
+    return new sdk::plugin::info{
+      .game_version = semver::version(),
+      .plugin_version = semver::version(),
+      .instance = &swp_template::instance()
+    };
+  }
+}
