6ba1c481d86f34ac6a21347dfe90497d5da3c0b6
sawich <sawich@angeldev.org>
2022-01-07 04:31:02 +0200

    update emplace method

--- swp-template/sources/emplace.cpp
+++ swp-template/sources/emplace.cpp
@@ -4,13 +4,10 @@
 // deps
 #include <sdk/plugin/plugin_emplace.hpp>
 
-namespace sdk::plugin
-{
-  auto emplace(void)->info* {
-    return new sdk::plugin::info{
-      .game_version = semver::version(),
-      .plugin_version = semver::version(),
-      .instance = &swp_template::instance()
-    };
-  }
+auto sdk::plugin::emplace(void)->sdk::plugin::info* {
+  return new sdk::plugin::info{
+    .game_version = semver::version(),
+    .plugin_version = semver::version(),
+    .instance = &swp_template::instance()
+  };
 }
