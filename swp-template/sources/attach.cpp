7f5a063bcf9ab41eb9b9afdc0d285f0b9d03f53c
sawich <sawich@angeldev.org>
2022-04-13 00:00:46 +0300

    change default plugin version

--- swp-template/sources/attach.cpp
+++ swp-template/sources/attach.cpp
@@ -7,7 +7,7 @@
 auto swpsdk::plugin::attach(void)->swpsdk::plugin::info* {
   return new swpsdk::plugin::info{
     .game_version = version{1, 4, 2, 3},
-    .plugin_version = version{},
+    .plugin_version = version{1},
     .instance = &swp_template::plugin::instance()
   };
 }
